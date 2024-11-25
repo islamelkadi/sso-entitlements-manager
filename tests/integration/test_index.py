7b23d703e04e93661d94f41da1e230270becf058
islamelkadi <ielkadi@torontomu.ca>
2024-11-25 15:38:36 -0500

    WIP

--- tests/integration/test_index.py
+++ tests/integration/test_index.py
@@ -9,7 +9,6 @@ from tests.utils import upload_file_to_s3
 
 # Constants
 BUCKET_NAME = "XXXXXXXXXXXXXX"
-LAMBDA_NAME = "XXXXXXXXXXXXXX"
 
 CWD = os.path.dirname(os.path.realpath(__file__))
 MANIFEST_SCHEMA_DEFINITION_FILEPATH = os.path.join(CWD, "..", "..", "src", "app", "schemas", "manifest_schema_definition.json")
@@ -29,4 +28,5 @@ VALID_MANIFEST_DEFINITION_FILES = [x for x in glob.glob(VALID_MANIFEST_DEFINITIO
 def test(setup_live_aws_environment: Dict[str, Any], manifest_filename: str):
     sts_client = boto3.client("sts")
     account_number = sts_client.get_caller_identity()["Account"]
+
     pass
