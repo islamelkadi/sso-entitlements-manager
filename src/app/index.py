d320f86eed987ea6da7ace3c4d6ddbf00837224e
islamelkadi <ielkadi@torontomu.ca>
2024-12-29 01:01:26 -0500

    WIP: cleanup & reorganization

--- src/app/index.py
+++ src/app/index.py
@@ -1,17 +1,4 @@
-"""
-Regex based rules engine for processing regex input for the
-purpose of assiging permission sets.
-"""
 import os
-import sys
-
-from aws_lambda_powertools import Logger, Tracer
-from aws_lambda_powertools.logging import correlation_paths
-from aws_lambda_powertools.utilities.typing import LambdaContext
-from aws_lambda_powertools.utilities.data_classes import EventBridgeEvent, event_source
-
-sys.path.append(os.path.join(os.path.dirname(__file__)))
-
 from app.core.utils import download_file_from_s3
 from app.services.access_manifest_file_reader import AccessManifestReader
 from app.services.aws_organizations_mapper import AwsOrganizationsMapper
@@ -21,16 +8,8 @@ from app.services.aws_identity_center_manager import AwsIdentityCenterManager
 MANIFEST_FILE_S3_LOCATION = os.getenv("MANIFEST_FILE_S3_LOCATION")
 MANIFEST_SCHEMA_DEFINITION_FILEPATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "schemas", "manifest_schema_definition.json")
 
-# AWS Lambda powertool objects & class instances
-TRACER = Tracer()
-LOGGER = Logger()
-
-
 # Lambda handler
-@TRACER.capture_lambda_handler
-@event_source(data_class=EventBridgeEvent)  # pylint: disable=E1120
-@LOGGER.inject_lambda_context(log_event=True, correlation_id_path=correlation_paths.EVENT_BRIDGE)
-def lambda_handler(event: EventBridgeEvent, context: LambdaContext):  # pylint: disable=W0613
+def lambda_handler(event, context):  # pylint: disable=W0613
     """
     Function to create or retrieve regex rules for SSO permission
     set assignments
@@ -52,7 +31,7 @@ def lambda_handler(event: EventBridgeEvent, context: LambdaContext):  # pylint: 
 
     # Download manifest file
     manifest_file_local_path = download_file_from_s3(MANIFEST_FILE_S3_LOCATION)
-
+    
     # Process manifest file
     manifest_file = AccessManifestReader()
     setattr(manifest_file, "schema_definition_filepath", MANIFEST_SCHEMA_DEFINITION_FILEPATH)
@@ -75,10 +54,10 @@ def lambda_handler(event: EventBridgeEvent, context: LambdaContext):  # pylint: 
     setattr(identity_center_manager, "ou_accounts_map", aws_org.ou_accounts_map)
     identity_center_manager.run_access_control_resolver()
 
-    LOGGER.info("Lambda execution complete")
-    LOGGER.info("Created %s assignments", len(identity_center_manager.assignments_to_create))
-    LOGGER.info("Deleted %s assignments", len(identity_center_manager.assignments_to_delete))
-    LOGGER.info("Invalid %s assignments", len(identity_center_manager.invalid_manifest_rules_report))
+    # LOGGER.info("Lambda execution complete")
+    # LOGGER.info("Created %s assignments", len(identity_center_manager.assignments_to_create))
+    # LOGGER.info("Deleted %s assignments", len(identity_center_manager.assignments_to_delete))
+    # LOGGER.info("Invalid %s assignments", len(identity_center_manager.invalid_manifest_rules_report))
 
     return {
         "created": identity_center_manager.assignments_to_create,
