f754bbb631c12038ccd8745478f5bbb4a6c5cb7f
islamelkadi <153553770+islamelkadi@users.noreply.github.com>
2025-01-19 00:20:38 -0500

    Update utils.py

--- src/services/aws/utils.py
+++ src/services/aws/utils.py
@@ -1,9 +1,14 @@
 import logging
 import functools
 from typing import Callable, Any
+
+import boto3
 from src.core.constants import SSO_ENTITLMENTS_APP_NAME
 
 logger = logging.getLogger(SSO_ENTITLMENTS_APP_NAME)
+sso_admin_client = boto3.client("sso-admin")
+aws_organization_client = boto3.client("organizations")
+
 
 def handle_aws_exceptions(func: Callable) -> Callable:
     """
@@ -23,13 +28,29 @@ def handle_aws_exceptions(func: Callable) -> Callable:
     def wrapper(self, *args, **kwargs) -> Any:
         try:
             return func(self, *args, **kwargs)
-        except self._organizations_client.exceptions.ParentNotFoundException as e:
+
+        # AWS Organizations related exceptions
+        except aws_organization_client.exceptions.ParentNotFoundException as e:
             logger.error(f"Invalid parent OU name: {e}")
             raise e
-        except self._organizations_client.exceptions.AccessDeniedException as e:
+        except aws_organization_client.exceptions.AccessDeniedException as e:
             logger.error(f"Missing required IAM policy permissions: {e}")
             raise e
-        except Exception as e:
-            logger.error(f"Unexpected error in {func.__name__}: {e}")
+        except aws_organization_client.exceptions.ServiceException as e:
+            logger.error(f"Unexpected service internal error: {e}")
+            raise e
+
+        # AWS SSO Admin related exceptions
+        except sso_admin_client.exceptions.AccessDeniedException as e:
+            logger.error(f"Missing required IAM policy permissions: {e}")
+            raise e
+        except sso_admin_client.exceptions.ServiceQuotaExceededException as e:
+            logger.error(f"Exceeded limit of allowed AWS account assignments, request service quota increase: {e}")
+            raise e
+        except sso_admin_client.exceptions.ResourceNotFoundException as e:
+            logger.error(f"Invalid TargetID, PrincipalID, or PermissionSetArn: {e}")
+            raise e
+        except sso_admin_client.exceptions.InternalServerException as e:
+            logger.error(f"Unexpected service internal server error: {e}")
             raise e
     return wrapper
