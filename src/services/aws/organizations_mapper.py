8cf17e69218ffcbfb3601ccde06fdc2bd3023e3d
islamelkadi <153553770+islamelkadi@users.noreply.github.com>
2025-01-18 23:48:19 -0500

    Update organizations_mapper.py
    Added AWS organizations exceptions

--- src/services/aws/organizations_mapper.py
+++ src/services/aws/organizations_mapper.py
@@ -40,7 +40,9 @@ OrganizationsMapper
         Runs all mapping methods to update OUs and accounts.
 """
 
+import logging
 import boto3
+from src.core.constants import SSO_ENTITLMENTS_APP_NAME
 
 
 class OrganizationsMapper:
@@ -95,6 +97,7 @@ class OrganizationsMapper:
         ------
         aws_orgs = OrganizationsMapper("root-ou-id", ["ExcludeOU1"], ["ExcludeAccount1"])
         """
+        self._logger = logging.getLogger(SSO_ENTITLMENTS_APP_NAME)
         self.exclude_ou_name_list: list = []
         self.exclude_account_name_list: list = []
         self.account_name_id_map: dict = {}
@@ -121,11 +124,23 @@ class OrganizationsMapper:
         parent_ou_id = parent_ou_id if parent_ou_id else self.root_ou_id
         ou_paginator = self._organizations_client.get_paginator("list_organizational_units_for_parent")
         aws_ou_iterator = ou_paginator.paginate(ParentId=parent_ou_id)
-        for page in aws_ou_iterator:
-            aws_ous_flattened_list.extend(page["OrganizationalUnits"])
+
+        try:
+            for page in aws_ou_iterator:
+                aws_ous_flattened_list.extend(page["OrganizationalUnits"])
+        except self._organizations_client.exceptions.ParentNotFoundException as e:
+            self._logger.error(f"Invalid parent OU name: {e}")
+            raise e
+        except self._organizations_client.exceptions.AccessDeniedException as e:
+            self._logger.error(f"Missing required IAM policy permissions: {e}")
+            raise e
+        except Exception as e:
+            self._logger.error(f"Unexpected error: {e}")
+            raise e
 
         for ou in aws_ous_flattened_list:
             if ou["Name"] not in self.exclude_ou_name_list and ou["Name"] not in self._ou_name_id_map:
+                self._logger.info(f"Traversing non-excluded parent AWS OU: {ou['Name']}")
                 self._map_aws_organizational_units(ou["Id"])
                 self._ou_name_id_map[ou["Name"]] = ou["Id"]
         self._ou_name_id_map["root"] = self.root_ou_id
@@ -144,11 +159,24 @@ class OrganizationsMapper:
             self.ou_accounts_map[ou_name] = []
             accounts_iterator = accounts_paginator.paginate(ParentId=ou_id)
             aws_accounts_flattened_list = []
-            for page in accounts_iterator:
-                aws_accounts_flattened_list.extend(page["Accounts"])
 
+            try:
+                for page in accounts_iterator:
+                    aws_accounts_flattened_list.extend(page["Accounts"])
+            except self._organizations_client.exceptions.ParentNotFoundException as e:
+                self._logger.error(f"Invalid parent OU name: {e}")
+                raise e
+            except self._organizations_client.exceptions.AccessDeniedException as e:
+                self._logger.error(f"Missing required IAM policy permissions: {e}")
+                raise e
+            except Exception as e:
+                self._logger.error(f"Unexpected error: {e}")
+                raise e
+
+            self._logger.info(f"{ou_name} OU contains {len(aws_accounts_flattened_list)} accounts, creating {ou_name}'s account itenerary...")
             for account in aws_accounts_flattened_list:
                 if account["Status"] == "ACTIVE" and account["Name"] not in self.exclude_account_name_list:
+                    self._logger.info(f"Appending non-excluded AWS account to {ou_name}'s itenerary")
                     self.ou_accounts_map[ou_name].append({"Id": account["Id"], "Name": account["Name"]})
 
     def _map_aws_accounts(self) -> None:
@@ -175,6 +203,11 @@ class OrganizationsMapper:
         ------
         self.run_ous_accounts_mapper()
         """
+        self._logger.info("Creating AWS OU names to ID map")
         self._map_aws_organizational_units(self.root_ou_id)
+
+        self._logger.info("Creating AWS OU names to account details map")
         self._map_aws_ou_to_accounts()
+
+        self._logger.info("Create AWS account name to ID map")
         self._map_aws_accounts()
